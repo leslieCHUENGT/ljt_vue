3c82a1c46c1b6f63341b54f46e8ac67f31db84ca
leslieCHUENGT <1278207976@qq.com>
2023-08-02 09:59:38 +0800

    fix:架构变更

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,20 @@
-import typescript from "@rollup/plugin-typescript";
-import sourceMaps from "rollup-plugin-sourcemaps";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import replace from "@rollup/plugin-replace";
 
+import typescript from '@rollup/plugin-typescript';
 export default {
-  input:"./packages/vue/src/index.ts",
-  plugins: [
-    replace({
-      "process.env.NODE_ENV": JSON.stringify("development"),
-      "process.env.VUE_ENV": JSON.stringify("browser"),
-      "process.env.LANGUAGE": JSON.stringify(process.env.LANGUAGE),
-    }),
-    resolve(),
-    commonjs(),
-    typescript(),
-    sourceMaps(),
-  ],
+  input: './packages/vue/src/index.ts',
   output: [
+    // 1.cjs-> commonjs
+    // 2.esm
     {
-      format: "cjs",
-      file: "./packages/vue/dist/mini-vue.cjs.js",
-      sourcemap: true,
+      format: 'cjs',
+      file: 'packages/vue/dist/vue.cjs.js'
     },
     {
-      name: "vue",
-      format: "es",
-      file: "./packages/vue/dist/mini-vue.esm-bundler.js",
-      sourcemap: true,
-    },
-  ],
-  onwarn: (msg, warn) => {
-    // 忽略 Circular 的错误
-    if (!/Circular/.test(msg)) {
-      warn(msg);
+      format: 'es',
+      file: 'packages/vue/dist/vue.esm.js'
     }
-  },
-};
+  ],
+  plugins: [
+    typescript()
+  ]
+}
