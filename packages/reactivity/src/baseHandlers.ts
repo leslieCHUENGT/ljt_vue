fee8503387ba4e256c24496f849f43014a7e0df8
leslieCHUENGT <1278207976@qq.com>
2023-08-03 09:36:10 +0800

    fix：更新响应式内容

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -3,21 +3,30 @@ import { ITERATE_KEY, pauseTracking, resetTracking, track, trigger } from "./eff
 import { TriggerOpTyes } from "./operations"
 import { reactive, ReactiveFlags, readonly, toRaw } from "./reactive"
 
+// 创建默认的getter和setter
 const get = createGetter()
 const set = createSetter()
 
+// 创建只读的getter
 const readonlyGet = createGetter(true)
+
+// 创建浅只读的getter
 const shallowReadonlyGet = createGetter(true, true)
 
-
+// 创建数组操作的函数
 const arrayInstrumentations = createArrayInstrumentations()
 function createArrayInstrumentations() {
   const instrumentations = {}
+
+    // 遍历需要处理的数组操作函数
+    // 例如：includes、indexOf、lastIndexOf等
     ;['includes', 'indexOf', 'lastIndexOf'].forEach(key => {
+      // 将数组操作函数包装成代理对象的方法
       instrumentations[key] = function (...args) {
         const arr = toRaw(this)
-        //this 是代理对象， 先在代理对象中查找  结果存在res中 
+        // 在原始对象中执行数组操作函数
         let res = arr[key](...args)
+        // 如果操作函数返回-1或false，则表示在代理对象中未找到，需要在原始对象中再次执行
         if (res === -1 || res === false) {
           return arr[key](...args.map(toRaw))
         } else {
@@ -26,10 +35,16 @@ function createArrayInstrumentations() {
       }
     })
 
+    // 遍历需要处理的数组操作函数
+    // 例如：push、pop、shift、unshift、splice等
     ;['push', 'pop', 'shift', 'unshift', 'splice'].forEach(key => {
+      // 将数组操作函数包装成代理对象的方法
       instrumentations[key] = function (...args) {
+        // 暂停追踪依赖
         pauseTracking()
+        // 在原始对象中执行数组操作函数
         const res = toRaw(this)[key].apply(this, args)
+        // 重置追踪依赖
         resetTracking()
 
         return res
@@ -39,12 +54,12 @@ function createArrayInstrumentations() {
   return instrumentations
 }
 
-
-
+// 创建getter函数
 function createGetter(isReadonly = false, shallow = false) {
   return function get(target, key) {
     const res = Reflect.get(target, key)
 
+    // 处理特殊的内部标记属性
     if (key === ReactiveFlags.IS_REACTIVE) {
       return !isReadonly
     } else if (key === ReactiveFlags.IS_READONLY) {
@@ -53,46 +68,53 @@ function createGetter(isReadonly = false, shallow = false) {
       return target
     }
 
+    // 如果是代理的数组对象，并且需要处理的是数组操作函数，则返回对应的包装方法
     if (isArray(target) && hasOwn(arrayInstrumentations, key)) {
       return Reflect.get(arrayInstrumentations, key)
     }
 
+    // 如果是浅只读，直接返回值
     if (shallow) {
       return res
     }
 
+    // 如果是可变的对象，并且不是只读的，需要追踪依赖
     if (!isReadonly) {
-      // 如果是嵌套对象 则需遍历执行reactive
       track(target, key)
     }
 
+    // 如果返回的值仍然是对象，则根据只读或可变状态进行递归处理
     if (isObject(res)) {
       return isReadonly ? readonly(res) : reactive(res)
     }
+
     return res
   }
 }
 
-
+// 创建setter函数
 function createSetter() {
   return function set(target, key, val, recevier) {
     const oldValue = target[key]
     const hadKey = isArray(target) ? Number(key) < target.length : hasOwn(target, key)
     const res = Reflect.set(target, key, val)
 
+    // 如果修改的是目标对象本身
     if (target === toRaw(recevier)) {
+      // 如果属性之前不存在，则触发ADD操作
       if (!hadKey) {
         trigger(target, TriggerOpTyes.ADD, key)
+        // 如果属性值发生改变，则触发SET操作
       } else if (hasChanged(val, oldValue)) {
         trigger(target, TriggerOpTyes.SET, key)
       }
     }
 
-
     return res
   }
 }
 
+// 删除属性时触发事件
 function deleteProperty(target, key) {
   const hadKey = hasOwn(target, key)
 
@@ -105,19 +127,20 @@ function deleteProperty(target, key) {
   return result
 }
 
-
+// 判断属性是否存在时触发事件
 function has(target, key) {
-
   const result = Reflect.has(target, key)
   track(target, key)
   return result
 }
 
-
+// 获取对象的键列表时触发事件
 function ownKeys(target) {
   track(target, isArray(target) ? 'length' : ITERATE_KEY)
   return Reflect.ownKeys(target)
 }
+
+// 可变对象的处理器
 export const mutableHandler = {
   get,
   set,
@@ -126,6 +149,7 @@ export const mutableHandler = {
   ownKeys
 }
 
+// 只读对象的处理器
 export const readonlyHandler = {
   get: readonlyGet,
   set(target, key, val) {
@@ -140,7 +164,7 @@ export const readonlyHandler = {
   }
 }
 
+// 浅只读对象的处理器
 export const shallowReadonlyHandlers = extend({}, readonlyHandler, {
   get: shallowReadonlyGet
 })
-
--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,62 +1,71 @@
 import { isObject } from "@mick-vue/shared"
 import { mutableHandler, readonlyHandler, shallowReadonlyHandlers } from "./baseHandlers"
 
-
+// 响应式标志枚举
 export const enum ReactiveFlags {
-  IS_REACTIVE = '__v_isReactive',
-  IS_READONLY = '__v_isReadonly',
-  RAW = '__v_raw'
+  IS_REACTIVE = '__v_isReactive', // 标记对象是响应式的
+  IS_READONLY = '__v_isReadonly', // 标记对象是只读的
+  RAW = '__v_raw' // 存储原始对象
 }
 
+// WeakMap用于存储代理对象和原始对象的对应关系
 const reactiveMap = new WeakMap()
 const readonlyMap = new WeakMap()
 const shallowReadonlyMap = new WeakMap()
 
+// 根据原始对象创建响应式对象
 export function reactive(raw) {
   return createReactiveObject(raw, mutableHandler, reactiveMap)
 }
 
+// 根据原始对象创建只读响应式对象
 export function readonly(raw) {
   return createReactiveObject(raw, readonlyHandler, readonlyMap)
 }
 
+// 根据原始对象创建浅只读响应式对象
 export function shallowReadonly(raw) {
   return createReactiveObject(raw, shallowReadonlyHandlers, shallowReadonlyMap)
 }
 
+// 判断对象是否是响应式的
 export function isReactive(value) {
   return !!value[ReactiveFlags.IS_REACTIVE]
 }
 
+// 判断对象是否是只读的
 export function isReadonly(value) {
   return !!value[ReactiveFlags.IS_READONLY]
 }
 
+// 判断对象是否是代理对象（响应式或只读）
 export function isProxy(value) {
   return isReactive(value) || isReadonly(value)
 }
 
+// 获取代理对象对应的原始对象
 export function toRaw(observed) {
   const raw = observed && observed[ReactiveFlags.RAW]
   return raw ? toRaw(raw) : observed
 }
 
-
+// 根据原始对象创建代理对象
 function createReactiveObject(target, baseHandlers, proxyMap) {
+  // 判断target是否是一个对象
   if (!isObject(target)) {
     console.warn(`target ${target}必须是一个对象`);
     return target
   }
 
-  // 优先通过原始对象Obj寻找之前创建的代理对象， 如果找到了，直接返回已有的代理对象
+  // 查找之前是否已经创建过target的代理对象，如果找到了，直接返回已有的代理对象
   const existingProxy = proxyMap.get(target)
   if (existingProxy) {
     return existingProxy
   }
 
-
+  // 创建Proxy代理对象
   const proxy = new Proxy(target, baseHandlers)
-  // 存储到Map中，从而避免重复创建
+  // 存储到map中，避免重复创建
   proxyMap.set(target, proxy)
 
   return proxy
